22ac4c931d13e4cc18456429a7a4dfcd836d86b9
Igor Sousa <igor2001silva3@gmail.com>
2024-05-30 00:07:05 -0300

    modulo 2 - exercicios 2 e 3

---
+++ js-completo-es6/modulo02-js-para-iniciantes/numeros-operadores/exercicios.js
@@ -0,0 +1,25 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // Resultado = 35
+console.log(total);
+
+// Crie duas expressões que retornem NaN
+var nome = 'Igor';
+var idade = 23;
+var soma = nome / idade;
+console.log(soma);
+
+// Somar a string '200' com o número 50 e retornar 250
+var num1 = '200';
+console.log(+num1 + 50);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var ii = 5;
+console.log(++ii);
+
+// Como dividir o peso por 2?
+var numero = '80';
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+//var pesoPorDois = peso / 2; // NaN (Not a Number)
+var pesoPorDois = numero / 2;
+console.log(pesoPorDois);
---
+++ js-completo-es6/modulo02-js-para-iniciantes/tipos-dados/exercicios.js
@@ -0,0 +1,19 @@
+// Declare uma variável contendo uma string
+var name = 'Igor';
+
+// Declare uma variável contendo um número dentro de uma string
+var age = '23';
+
+// Declare uma variável com a sua idade
+var myAge = 23;
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var firstName = 'Igor';
+var lastName = 'Sousa';
+console.log(firstName + ' ' + lastName);
+
+// Coloque a seguinte frase em uma variável: It's time
+var phrase = `It's time`;
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof firstName);
