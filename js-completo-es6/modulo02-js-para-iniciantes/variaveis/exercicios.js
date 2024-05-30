51059928a39c661d0015281514b8349c34e0512d
Igor Sousa <igor2001silva3@gmail.com>
2024-05-29 23:37:05 -0300

    exercicio 1 - variaveis

---
+++ js-completo-es6/modulo02-js-para-iniciantes/variaveis/exercicios.js
@@ -0,0 +1,14 @@
+//Declarar uma variável com o seu nome
+var name = 'Igor';
+
+//Declarar uma variável com a sua idade
+var age = 23;
+
+//Declarar uma variável com a sua comida favorita e não atribuir valor
+var favoriteFood;
+
+//Atribuir valor a sua comida favorita
+favoriteFood = 'Pizza';
+
+//Declarar 5 variáveis diferentes sem valores
+var first, second, third, fourth, fifth;
